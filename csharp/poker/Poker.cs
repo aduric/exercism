6d7e6a09dfa34d06d4f61fc94cbe39ac1f4b24fd
aduric <adnan.duric@d2l.com>
2018-02-23 16:07:24 -0500

    Basic hand mapping

--- csharp/poker/Poker.cs
+++ csharp/poker/Poker.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using Poker.Deck;
 using Poker.Hand;
+using Rank = Poker.Deck.Rank;
 
 namespace Poker {
     public static class Poker {
@@ -15,8 +17,46 @@ namespace Poker {
 
         public static List<IHand> MapHands( IEnumerable<string> hands ) {
             // map strings to IHand types
+            bool isTrips = false;
+            bool isPair1 = false;
+            bool isPair2 = false;
 
-            return new List<IHand>();
+            var handObjs = new List<IHand>();
+
+            foreach( var hand in hands ) {
+                IHand handObj;
+                var handBuff = new List<Card>();
+                foreach( var cardStr in hand ) {
+                    var currCard = new Card( Rank.Ace, Suit.Spade );
+
+                    var dupCount = handBuff.Count(c => c.Rank == currCard.Rank);
+                    if( dupCount == 2 ) {
+                        isTrips = true;
+                    } else if( dupCount == 1 ) {
+                        if( isPair1 ) {
+                            isPair2 = true;
+                        } else {
+                            isPair1 = true;
+                        }
+                    }
+
+                    handBuff.Add( currCard );
+                }
+
+                if( isTrips && isPair1 ) {
+                    handObj = new FullHouse( handBuff );
+                } else if( isTrips ) {
+                    handObj = new Triple( handBuff );
+                } else if( isPair1 ) {
+                    handObj = new Pair( handBuff );
+                } else {
+                    handObj = new None( handBuff );
+                }
+
+                handObjs.Add( handObj );
+            }
+
+            return handObjs;
         }
 
     }
